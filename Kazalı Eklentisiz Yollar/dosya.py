c21e26c2045eb8046b79eb3929f9a13ab124521c
mehmetcanyenikan <48149867+mehmetcanyenikan@users.noreply.github.com>
2021-12-26 12:39:09 +0300

    Grafikler

---
+++ Kazalı Eklentisiz Yollar/dosya.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 25 12:22:05 2021
+
+@author: Can
+"""
+
+import shutil
+import os
+for k in range(0,110,10):
+    for j in range(0,3):
+        os.mkdir('1200-{}-{}'.format(k,j))
+        os.listdir()
+        ['test']
+    
+import shutil
+import os
+for k in range(0,110,10):
+    for j in range(0,3):
+        os.mkdir('1350-{}-{}'.format(k,j))
+        os.listdir()
+        ['test']
+        
+        
+import shutil
+import os
+for k in range(0,110,10):
+    for j in range(0,3):
+        os.mkdir('1500-{}-{}'.format(k,j))
+        os.listdir()
+        ['test']
---
+++ Kazasız Eklentili Yol/dosya.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 25 12:22:05 2021
+
+@author: Can
+"""
+
+import shutil
+import os
+for k in range(1):
+    for j in range(7):
+
+        os.mkdir('600-150-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+
+        os.mkdir('600-200-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+
+        os.mkdir('600-250-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+    
+    
+
+
+        os.mkdir('750-150-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+    
+
+
+        os.mkdir('750-200-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+
+
+        os.mkdir('750-250-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+
+
+        os.mkdir('900-150-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+       
+        
+
+
+        os.mkdir('900-200-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+
+
+        os.mkdir('900-250-{}-{}'.format(j,k))
+        os.listdir()
+        ['test']
+        
+    
